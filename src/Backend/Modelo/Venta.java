3a876e963b94f511bcd25816c55b8c98e0283dd6
César Antonio Navarro Sosa <cesaaar26@gmail.com>
2020-11-20 15:21:38 -0600

    Cesar
    más pruebas

--- src/Backend/Modelo/Venta.java
+++ src/Backend/Modelo/Venta.java
@@ -1,7 +1,10 @@
 package Backend.Modelo;
 
 public class Venta {
-    //vamos a ver que esto jale al 100 7u7
-    //saludos 
-    //creo que ya quedo bine recio esta madre
+    // vamos a ver que esto jale al 100 7u7
+    // saludos
+    // creo que ya quedo bine recio esta madre
+    public static void main(String[] args) {
+        System.out.println("Hola mundo xS");
+    }
 }
