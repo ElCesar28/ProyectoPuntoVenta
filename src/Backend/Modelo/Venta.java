8a5be21c76e754671260532605ad4961e0ad1828
César Antonio Navarro Sosa <cesaaar26@gmail.com>
2020-11-20 15:28:04 -0600

    Update Venta.java

--- src/Backend/Modelo/Venta.java
+++ src/Backend/Modelo/Venta.java
@@ -1,10 +1,65 @@
 package Backend.Modelo;
 
+import java.util.Date;
+
+
+
 public class Venta {
-    // vamos a ver que esto jale al 100 7u7
-    // saludos
-    // creo que ya quedo bine recio esta madre
-    public static void main(String[] args) {
-        System.out.println("Hola mundo xS");
+    private int idVenta;
+    private Date fecha;
+    private double total;
+    private int idCliente;
+    private int idEmpleado;
+
+    public Venta(int idVenta, Date fecha, double total, int idCliente, int idEmpleado) {
+        this.idVenta = idVenta;
+        this.fecha = fecha;
+        this.total = total;
+        this.idCliente = idCliente;
+        this.idEmpleado = idEmpleado;
     }
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+    
+    
+    
+    
 }
